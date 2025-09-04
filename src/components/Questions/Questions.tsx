913ec4328ffa4cdd4e0aaa7b5b8e00998ea518f2
German Bilalov <pinncho@gmail.com>
2025-09-04 23:34:28 +0200

    style: improve title styles

--- src/components/Questions/Questions.tsx
+++ src/components/Questions/Questions.tsx
@@ -33,10 +33,15 @@ export default function Questions() {
 
   return (
     <div className="flex flex-col justify-between h-full max-w-6xl mx-auto">
-      <div className="flex justify-between items-center my-8">
-        <h3 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-700 dark:text-gray-300">
-          Krok 2, Dentistry, 2023
-        </h3>
+      <div className="flex justify-between items-center my-8 gap-2">
+        <div className="flex flex-col">
+          <h3 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-700 dark:text-gray-300">
+            Krok 2
+          </h3>
+          <span className="font-medium text-gray-600 dark:text-gray-400">
+            Dentistry, 2023
+          </span>
+        </div>
         <div className="flex items-center p-1 rounded-full bg-white dark:bg-gray-800 gap-2">
           <button
             className={`button-view ${view === "single" ? "active" : ""}`}
